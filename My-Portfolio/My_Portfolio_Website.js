224ed95d0f00d4bc3a502b4300b5a9f28356c2d4
Lewis <lewis.c.warren@gmail.com>
2019-12-06 10:26:31 -0800

    Push updated portfolio website

---
+++ My-Portfolio/My_Portfolio_Website.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+    $("#contactForm").css("display", "none")
+});
+
+function openForm() {
+    document.getElementById("contactForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("contactForm").style.display = "none";
+}
+
